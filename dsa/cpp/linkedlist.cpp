11a83a538db7f3ba6f169fce11b2d84b6b57a997
useraid <useraid@pm.me>
2022-10-05 23:44:55 +0530

    linked list cpp

---
+++ dsa/cpp/linkedlist.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+Node* head;
+
+void Insert(int x){
+    
+}
+
+int main(){
+    head = NULL;
+    cout << "How many Numbers? ";
+    int n,x;
+    cin >> n;
+    for (int i=0, i<n, i++){
+        cout << "Enter Number ";
+        cin >> x;
+    }
+}
