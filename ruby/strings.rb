e80e912edeb93b992d232f04ec57b3a2cd4b747d
useraid <useraid@pm.me>
2022-10-07 23:05:24 +0530

    added ruby

---
+++ ruby/strings.rb
@@ -0,0 +1,8 @@
+line = "top string"
+puts line.upcase()
+puts line.downcase()
+puts line.length()
+puts line[2]
+puts line[0.2]
+puts line.index("op")
+puts "hello".upcase()
---
+++ ruby/variables.rb
@@ -0,0 +1,4 @@
+name = "user"
+age = "35"
+puts ("Hello, " + name)
+puts ("You are " + age + " years old.")
